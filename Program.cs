80d47335ba7fdb3459600a71401408b6c578201c
tarun <tarunrk2002@gmail.com>
2024-08-02 12:25:08 +0530

    builder settings

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿namespace mmmmmcore
+﻿using Microsoft.Extensions.Configuration;
+
+namespace mmmmmcore
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
 
+        }
+
+        public static void BuildConfig(IConfigurationBuilder builder)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("appsettings.json",optional:false,reloadOnChange:true);
+            builder.AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENR") ?? "production"}.json",optional: true);
+            builder.AddEnvironmentVariables();
         }
 
     }
